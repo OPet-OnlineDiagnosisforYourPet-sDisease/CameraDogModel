b551d0399ec775000fa04856a730b795c7a1d8c9
rozisaputra24 <82568840+rozisaputra24@users.noreply.github.com>
2023-06-07 09:09:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, UploadFile, File
 import tensorflow as tf
 import numpy as np
@@ -5,20 +6,28 @@ import os
 from tensorflow.keras.preprocessing import image
 import shutil
 from pydantic import BaseModel
+import mysql
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 app = FastAPI()  # create a new FastAPI app instance
 
+# port = int(os.getenv("PORT"))
+port = 8000
+
 model = tf.keras.models.load_model('SkinDisease.h5')
 
 def predict(file):
     img = image.load_img(file, target_size=(150,150))
+
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
+
+
     images = np.vstack([x]) 
     classes = model.predict(images) 
     print(classes[0]) 
+
     result = np.argmax(classes[0])
     
     resmessage = ""
@@ -32,6 +41,7 @@ def predict(file):
         resmessage = 'Ringworm'
 
     return resmessage
+
 
 # GCS Section
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./service_account.json"
@@ -69,29 +79,28 @@ def updatemysql(userID, petID, url):
 
 @app.get("/")
 def hello_world():
-    return "hello world"
+    return ("hello world")
 
 @app.post("/imageclassify")
-async def imageclassify(input: UploadFile = File(...)):
+def imageclassify(input: UploadFile = File(...)):
     savefile = input.filename
     with open(savefile, "wb") as buffer:
         shutil.copyfileobj(input.file, buffer)
     result = predict(savefile)
     gcs_url = uploadtogcs(savefile)
     os.remove(savefile)
-    return {"result": result, "gcs_url": gcs_url}
+    return (result, gcs_url)
 
 class Item(BaseModel):
     userID: int
     petID: int
     gcs_url: str
 
+
 @app.post("/updatedb")
 def updatedb(item: Item):
     updateddb = updatemysql(item.userID, item.petID, item.gcs_url)
-    return updateddb
+    return updatedb
 
 if __name__ == '__main__':
-    import uvicorn
-    port = int(os.environ.get('PORT', 8080))
-    uvicorn.run(app, host='0.0.0.0', port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, timeout_keep_alive=1200)
